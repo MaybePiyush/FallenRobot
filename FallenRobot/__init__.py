3054be99ece7b1edbad3e7c1b254030bbc2690c4
ᴘɪʏᴜꜱʜ <124165177+MaybePiyush@users.noreply.github.com>
2023-04-30 16:40:27 +0530

    Update __init__.py
    Signed-off-by: ᴘɪʏᴜꜱʜ <124165177+MaybePiyush@users.noreply.github.com>

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -45,10 +45,10 @@ if ENV:
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
     START_IMG = os.environ.get(
-        "START_IMG", "https://telegra.ph/file/40eb1ed850cdea274693e.jpg"
+        "START_IMG", "https://graph.org/file/c57d0e9034c2a0b37a6ff.jpg"
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "DevilsHeavenMF")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Chatting_Spot")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
@@ -142,7 +142,7 @@ else:
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(1356469075)
+DEV_USERS.add(5887412907)
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
